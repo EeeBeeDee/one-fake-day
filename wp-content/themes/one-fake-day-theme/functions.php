d3173d2f861cd5b54d669021de9cd621f8cf5edd
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-08-12 23:38:20 +0100

    Add colour vars

--- wp-content/themes/one-fake-day-theme/functions.php
+++ wp-content/themes/one-fake-day-theme/functions.php
@@ -1,11 +1,12 @@
 <?php
     
     function scripts() {
-        wp_register_style('style', get_template_directory_uri() . '/dist/app.css');
-        wp_enqueue_style('style');
-
+        
         wp_register_style('bootstrap-custom', get_template_directory_uri() . '/dist/bootstrap-custom.css');
         wp_enqueue_style('bootstrap-custom');
+        
+        wp_register_style('style', get_template_directory_uri() . '/dist/app.css');
+        wp_enqueue_style('style');
     }
 
     add_action('wp_enqueue_scripts', 'scripts')
