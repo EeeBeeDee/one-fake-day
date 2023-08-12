a7bf6bf7d62f7cbeb241a835baebad4cd1e1d5e6
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-08-13 00:52:45 +0100

    Add header skeleton and logo

--- wp-content/themes/one-fake-day-theme/header.php
+++ wp-content/themes/one-fake-day-theme/header.php
@@ -9,3 +9,22 @@
 </head>
 
 <body <?php body_class(); ?>>
+    <header class="site-header">
+        <div class="container h-100">
+            <div class="d-flex align-items-center justify-content-start h-100">
+                <div class="site-header__logobox d-flex align-items-center">
+                    <a class="d-flex align-items-center" href="#">
+                        <?php get_template_part('template-parts/logo') ?>
+                        <h2>One Fake Day</h2>
+                    </a>
+                </div>
+                <ul class="site-header__nav d-flex h-100 align-items-center">
+                    <li><a href="#">about</a></li>
+                    <li>blah</li>
+                    <li>contact</li>
+                    <li>yeeow</li>
+                </ul>
+
+            </div>
+        </div>
+    </header>
