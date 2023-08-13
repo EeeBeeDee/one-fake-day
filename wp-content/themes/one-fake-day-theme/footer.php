e69b9aa029de8369be7a63cfa998a8f355c1a585
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-08-13 21:47:30 +0100

    Finish social aniimations

--- wp-content/themes/one-fake-day-theme/footer.php
+++ wp-content/themes/one-fake-day-theme/footer.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="site-footer">
     <div class="container h-100">
-        <div class="site-footer__flex d-flex align-items-center  h-100 w-100">
+        <div class="site-footer__flex d-flex flex-column flex-md-row align-items-center  h-100 w-100">
             <div class="site-footer__logobox d-flex align-items-center">
                 <a class="d-flex align-items-center" href="#">
                     <?php get_template_part('template-parts/logo') ?>
@@ -52,7 +52,7 @@
                     <a href="">Hen Parties</a>
                 </li>
             </ul>
-            <ul class="site-footer__social">
+            <ul class="site-footer__social flex-row flex-md-column flex-lg-row">
                 <li>
                     <a class="facebook" href="#">
                         <span></span>
