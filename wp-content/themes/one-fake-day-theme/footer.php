d2ce0b92f83a6b0e88b489e246e2f62603531c96
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-08-13 20:27:07 +0100

    Add foot social + animation

--- wp-content/themes/one-fake-day-theme/footer.php
+++ wp-content/themes/one-fake-day-theme/footer.php
@@ -1,12 +1,12 @@
 <footer id="footer" class="site-footer">
     <div class="container h-100">
-        <div class="site-footer__flex d-flex align-items-center justify-content-between h-100">
+        <div class="site-footer__flex d-flex align-items-center  h-100 w-100">
             <div class="site-footer__logobox d-flex align-items-center">
                 <a class="d-flex align-items-center" href="#">
                     <?php get_template_part('template-parts/logo') ?>
                 </a>
             </div>
-            <ul class="site-footer__nav">
+            <ul class="site-footer__nav align-self-start">
                 <li class="site-footer__nav-links">
                     <a href="">About One Fab Day</a>
                 </li>
@@ -30,6 +30,64 @@
                 </li>
                 <li class="site-footer__nav-links">
                     <a href="">Privacy Policy</a>
+                </li>
+            </ul>
+            <ul class="site-footer__articles align-self-start ">
+                <li class="site-footer__article-head">
+                    <h2 class="text-uppercase m-0">articles</h2>
+                </li>
+                <li class="site-footer__article-links">
+                    <a href="">Real Weddings</a>
+                </li>
+                <li class="site-footer__article-links">
+                    <a href="">Wedding Dresses</a>
+                </li>
+                <li class="site-footer__article-links">
+                    <a href="">Honeymoons & Travels</a>
+                </li>
+                <li class="site-footer__article-links">
+                    <a href="">Wedding Planning</a>
+                </li>
+                <li class="site-footer__article-links">
+                    <a href="">Hen Parties</a>
+                </li>
+            </ul>
+            <ul class="site-footer__social">
+                <li>
+                    <a class="facebook" href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="twitter" href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="instagram" href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="google" href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <i class="fa fa-google-plus" aria-hidden="true"></i>
+                    </a>
                 </li>
             </ul>
         </div>
