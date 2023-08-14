478173a263de874f83470cffcba5628762c43feb
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-08-14 15:29:59 +0100

    Add banner ad skeleton

--- wp-content/themes/one-fake-day-theme/front-page.php
+++ wp-content/themes/one-fake-day-theme/front-page.php
@@ -12,23 +12,39 @@
                     'post_type' => 'post',
                     'posts_per_page' => -1
                 ));
+                $loopCount = 0;
+                $postCount = $blogPosts->post_count();
                 while ($blogPosts->have_posts()) {
-                    $blogPosts->the_post(); ?>
-                    <div class="col-5">
+                    $blogPosts->the_post(); 
+                    $loopCount++ ?>
+                    <div class="col-6">
                         <div class="post-card">
                             <img class="post-card__img" src="<?php echo get_the_post_thumbnail(); ?>" alt="">
                             <div class="post-card__infobox">
                                 <span class="post-card__category">
                                     <a href=""><?php the_category(); ?></a>
                                 </span>
-                                <h2 class="post-card__title">
-                                    <?php the_title(); ?>
+                                <h2 class="post-card__title p-3">
+                                    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
                                 </h2>
                             </div>
                         </div>
                     </div>
                  
-                <?php }
+                <?php
+                
+                if ($loopCount == 6 OR $loopCount == 12) {
+                    ?>
+                    <div class="col-12">
+                        <div class="banner-ad">
+                            
+                        </div>
+                    </div>
+               <?php }
+            
+            
+            
+            }
                 wp_reset_postdata();
                 ?>
             </div>
@@ -39,7 +55,7 @@
     </div>
 </div>
 
-
+<div style="height: 50vh;"></div>
 
 
 <?php get_footer(); ?>
