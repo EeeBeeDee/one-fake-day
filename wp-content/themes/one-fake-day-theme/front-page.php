66739a58912d74589c862a53683b67e243a9bb3b
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-08-14 14:46:29 +0100

    Add post-card img animation

--- wp-content/themes/one-fake-day-theme/front-page.php
+++ wp-content/themes/one-fake-day-theme/front-page.php
@@ -4,38 +4,42 @@
 
 <div class="container">
     <div class="row">
-        <?php
+        <div class="col-9">
+            <div class="row">
+                <?php
 
-            $blogPosts = new WP_Query(array(
-                'post_type' => 'post',
-                'posts_per_page' => -1
-            ));
-            while ($blogPosts->have_posts()) {
-                $blogPosts->the_post(); ?>
-                <div class="col-1"></div>
-                <div class="col-4">
-                    <div class="post-card">
-                        <img class="post-card__img" src="<?php echo get_the_post_thumbnail(); ?>" alt="">
-                        <div class="post-card__infobox">
-                            <span class="post-card__category">
-                                <?php the_category(); ?>
-                            </span>
-                            <h2 class="post-card__title">
-                                <?php the_title(); ?>
-                            </h2>
+                $blogPosts = new WP_Query(array(
+                    'post_type' => 'post',
+                    'posts_per_page' => -1
+                ));
+                while ($blogPosts->have_posts()) {
+                    $blogPosts->the_post(); ?>
+                    <div class="col-5">
+                        <div class="post-card">
+                            <img class="post-card__img" src="<?php echo get_the_post_thumbnail(); ?>" alt="">
+                            <div class="post-card__infobox">
+                                <span class="post-card__category">
+                                    <a href=""><?php the_category(); ?></a>
+                                </span>
+                                <h2 class="post-card__title">
+                                    <?php the_title(); ?>
+                                </h2>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-1"></div>
-           <?php }
-           wp_reset_postdata();
-        ?>
+                 
+                <?php }
+                wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+        <div class="col-3">
+            ADS
+        </div>
     </div>
 </div>
 
 
-    <div style="height: 200vh;">
-    </div>
+
 
 <?php get_footer(); ?>
-
